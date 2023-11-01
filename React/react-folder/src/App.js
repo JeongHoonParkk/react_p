a3c642c6be756d4942363addcf88bb8ccc2d5bab
DESKTOP-B79QV1H\USER <wjdgns5248@hanmail.net>
2023-11-01 17:25:36 +0900

    20231101

--- React/react-folder/src/App.js
+++ React/react-folder/src/App.js
@@ -1,4 +1,14 @@
-console.log(fetch('https://api.randomuser.me/?nat=US&results=1'));
+const createScream = logger => message => {
+  logger(message.toUppercase())
+}
+
+const scream = createScream(message => console.log(message));
+
+
+scream('함수가 함수 반환')
+scream('createScream은 함수 반환')
+scream('scream 은 createScream이 반환한 함수를 가리킴')
+
 
 
 function App() {
